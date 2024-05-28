027deeb0e85bd69e73b8e51c29dd44b452e9c461
Marshall <43077149+Marshall0703@users.noreply.github.com>
2024-05-28 14:14:55 -0400

    Update Contact.js

--- src/Content/Contact.js
+++ src/Content/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
         <h2>Contact Me</h2>
         <p className="smaller-text">Marshall Keyser</p>
         <span className="small-text">Phone Number 555-555-5555</span>
-        <p className="small-text">Email: Marshall@yourmom.com</p>
+        <p className="small-text">Email: Marshall@email.com</p>
         <span className="smaller-text">Social Links</span>
       </div>
       </div>
